5ec469c1918600f2280a13c8e8332cca7d43810e
babya <babya@ERIC_WEBER>
2022-05-05 12:07:14 +0300

    Add project files.

---
+++ Student/Login.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace Student
+{
+    class Account
+    {
+        string Login { get; set; }
+        string Password { get; set; }
+        string PasswordCheck { get; set; }
+        public bool SetLogin()
+        {
+            bool check = false;
+            var Lpattern = @"([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})";
+            do
+            {
+                Console.Write("\nEnter your email: ");
+                Login = Console.ReadLine();
+            } while (!Regex.IsMatch(Login, Lpattern));
+            check = true;
+            return check;
+        }
+        public bool SetPassword()
+        {
+            bool check = false;
+            var Ppattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}";
+            do
+            {
+                Console.Write("\nEnter your password: ");
+                Password = Console.ReadLine();
+                Console.Write("\nConfirm your password: ");
+                PasswordCheck = Console.ReadLine();
+            } while (Password != PasswordCheck || !Regex.IsMatch(Password, Ppattern));
+            check = true;
+            return check;
+        }
+        public void SetUser()
+        {
+            using (StreamWriter write = new StreamWriter("database.txt", true))
+            {
+                if (SetLogin() && SetPassword())
+                {
+                    write.WriteLine($"Login: {Login}\nPassword: {Password}");
+                }
+            }
+        }
+    }
+}
---
+++ Student/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string rPath = @"D:\C#\REGULAR\REGULAR\bin\Debug\Read.cs";
+           string wPath = "Write.cs";
+           List<string> text = new List<string>();
+           using (StreamReader file = new StreamReader(rPath,Encoding.UTF8))
+           {
+
+               while (file.EndOfStream == false)
+                   text.Add(file.ReadLine());
+           }
+
+           for (int i = 0; i < text.Count; i++)
+           {
+               text[i] = Regex.Replace(text[i], @"(?i)(?<!-)\bpublic\b", @"private");
+               text[i] = Regex.Replace(text[i], "[ ]+", " ");
+           }
+
+           using (StreamWriter writer = new StreamWriter(wPath, false))
+           {
+               foreach (var item in text)
+                   writer.WriteLine(item);
+
+           }*/
+            Account account = new Account();
+            account.SetUser();
+        }
+    }
+}
